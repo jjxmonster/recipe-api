aee9925360856475f6c7b65c0c41cfa4fe571813
jjxmonster <kuba.tarabasz@wp.pl>
2025-05-26 15:29:55 +0200

    feat: implement user registration and update user DTOs

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthService } from './auth/auth.service';
 import { AuthController } from './auth/auth.controller';
 import { UserService } from './user/user.service';
+import { User } from 'src/user/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [AuthService, UserService],
   controllers: [AuthController],
 })
--- src/auth/auth/auth.controller.ts
+++ src/auth/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from '../user/dto/create-user.dto';
+import { User } from 'src/user/user.entity';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  async register(@Body() user: CreateUserDto): Promise<User> {
+    // Todo: make sure that password & confirm password are equal
+    return this.authService.register(user);
+  }
+}
